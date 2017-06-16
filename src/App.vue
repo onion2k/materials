13360e6bde4f325edb82972d5add66f514b01820
Chris Neale <chris@komododigital.co.uk>
2017-06-16 08:46:49 +0100

    Material selector menu added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <h1>Three Material Builder</h1>
 
     <Material :materialId="this.materialId" :material="this.material"></Material>
-    <Editor   :materialId="this.materialId" v-on:change="change"></Editor>
+    <Editor   :materialId="this.materialId" :materials="this.materials" v-on:change="change"></Editor>
 
   </div>
 </template>
@@ -24,12 +24,12 @@ import Material from './components/Material.vue';
 import Editor from './components/Editor.vue';
 
 let materials = [
-  new MeshToonMaterial({ color: 0xff00ff }),
-  new MeshToonMaterial({ color: 0xff00ff, shininess: 50, shading: SmoothShading }),
-  new MeshPhysicalMaterial({ color: 0xff00ff, roughness: 0.5, metalness: 0.5, clearCoat: 0.1, shading: SmoothShading }),
-  new MeshPhysicalMaterial({ color: 0xff00ff, roughness: 0.5, metalness: 0.5, shading: SmoothShading }),
-  new MeshPhysicalMaterial({ color: 0xff00ff, roughness: 0.1, metalness: 0.1, shading: SmoothShading }),
-  new MeshPhongMaterial({ color: 0xff00ff, shininess: 100, shading: SmoothShading })
+    new MeshToonMaterial({ name:'Toon 1', color: 0xff00ff }),
+    new MeshToonMaterial({ name:'Toon 2', color: 0xff00ff, shininess: 50, shading: SmoothShading }),
+    new MeshPhysicalMaterial({ name:'Physical 1', color: 0xff00ff, roughness: 0.5, metalness: 0.5, clearCoat: 0.1, shading: SmoothShading }),
+    new MeshPhysicalMaterial({ name:'Physical 2', color: 0xff00ff, roughness: 0.5, metalness: 0.5, shading: SmoothShading }),
+    new MeshPhysicalMaterial({ name:'Physical 3', color: 0xff00ff, roughness: 0.1, metalness: 0.1, shading: SmoothShading }),
+    new MeshPhongMaterial({ name:'Phong 1', color: 0xff00ff, shininess: 100, shading: SmoothShading })
 ];
 
 export default {
@@ -37,7 +37,8 @@ export default {
   data () {
     return {
         materialId: 0,
-        material: materials[0]
+        material: materials[0],
+        materials: materials
     }
   },
   methods: {
