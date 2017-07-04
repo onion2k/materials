0dbf70d7f8ba99cbaddb8441239731dc2eafd7c4
Chris Neale <chris@komododigital.co.uk>
2017-07-04 08:04:33 +0100

    Images moved to module

--- src/components/mapLoader.vue
+++ src/components/mapLoader.vue
@@ -50,39 +50,7 @@
 
 import vueSlider from 'vue-slider-component';
 
-import Check from '../images/check.jpg';
-import Diamond from '../images/diamond.jpg';
-import Diagonal from '../images/diagonal.jpg';
-import Dots from '../images/dots.jpg';
-import Flagstones from '../images/flagstones.jpg';
-import Marble from '../images/marble.jpg';
-import Metal from '../images/metal.jpg';
-import Mud from '../images/mud.jpg';
-import Plates from '../images/plates.jpg';
-import Shingle from '../images/shingle.jpg';
-import Snake from '../images/snake.jpg';
-import Stone from '../images/stone.jpg';
-import Stone2 from '../images/stone2.jpg';
-import Stripes from '../images/stripes.png';
-import Wood from '../images/wood.jpg';
-
-let images = {
-    'Check': Check,
-    'Diamond': Diamond,
-    'Diagonal': Diagonal,
-    'Dots': Dots,
-    'Flagstones': Flagstones,
-    'Marble': Marble,
-    'Metal': Metal,
-    'Mud': Mud,
-    'Plates': Plates,
-    'Shingle': Shingle,
-    'Snake': Snake,
-    'Stone': Stone,
-    'Stone2': Stone2,
-    'Stripes': Stripes,
-    'Wood': Wood
-};
+import images from '../lib/images';
 
 export default {
   name: 'mapLoader',
