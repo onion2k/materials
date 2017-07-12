9f6e8895275fab994d16be262753068270e9a24d
Chris <chris@usablehq.com>
2017-07-12 09:45:21 +0100

    Updated webpack to use presets for babel

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
+      sourceMap: true,
       compress: {
         warnings: true
       }
